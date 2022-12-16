b87c69f3de7ca9b4a78b871b97a4b804d3ca21ba
leedohyeon0502 <leedohyeono0502@gmail.com>
2022-12-16 12:22:48 +0900

    dart ?? value 'null = value'

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,9 +12,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: _appBar(),
         body: Column(
           children: [Text("Theme Data", style: TextStyle(fontSize: 30))],
         ));
   }
+}
+
+_appBar() {
+  final s = GetStorage();
+  return AppBar(
+    leading: GestureDetector(
+      onTap: () {},
+      child: Icon(
+        Icons.nightlight_round,
+        size: 20,
+      ),
+    ),
+    actions: [
+      Icon(
+        Icons.person,
+        size: 20,
+      ),
+      SizedBox(width: 20),
+      Text((s.read('isDarkMode') ?? false).toString())
+    ],
+  );
 }
